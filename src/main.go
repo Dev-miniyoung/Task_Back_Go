b36c6bf940cbf6942e6778e5e31e9bb9d6324793
LeeYoungMin <miniyoung37@gmail.com>
2020-02-07 20:26:03 +0900

    add inventory get method

--- src/main.go
+++ src/main.go
@@ -1,14 +1,17 @@
 package main
 
 import (
-	"fmt" 
+	
 	"net/http"
-
+	"encoding/json"
+	// "log"
+	// "math/rand"
+	// "strconv"
 	"github.com/gorilla/mux"
 )
 
 // Model
-type Inventory struct{
+type Car struct{
 	No 		int 	`json:"no"`
 	Vin 	string 	`json:"vin"`
 	Model 	string 	`json:"model"`
@@ -24,65 +27,66 @@ var carData []Car
 
 // Get Inventory data
 func getInventory(w http.ResponseWriter, r *http.Request){
-
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(carData)
 }
 
-func main(){
+func main() {
 	// Init Router
 	r := mux.NewRouter()
 
 	// Dummy Data
 	carData = append(carData, Car{
-		No: "1",
+		No: 1,
 		Vin: "MNBUMF050FW496402",
 		Model: "320i",
 		Make: "BMW",
-		Year: "2014",
-		MSRP: "147,000",
+		Year: 2014,
+		MSRP: 147000,
 		Status: "ordered",
 		Booked: "y",
-		Listed: "n"
-	  })
+		Listed: "n",
+	})
 	carData = append(carData, Car{
-		No: "2",
+		No: 2,
 		Vin: "4JDBLMF080FW468775",
 		Model: "Camry",
 		Make: "Toyota",
-		Year: "2015",
-		MSRP: "120,000",
+		Year: 2015,
+		MSRP: 120000,
 		Status: "in stock",
 		Booked: "y",
-		Listed: "n"
-	  })
+		Listed: "n",
+	})
 	carData = append(carData, Car{
-		No: "1",
+		No: 3,
 		Vin: "MNBUMF050FW496402",
 		Model: "320i",
 		Make: "BMW",
-		Year: "2014",
-		MSRP: "147,000",
+		Year: 2014,
+		MSRP: 147000,
 		Status: "ordered",
 		Booked: "y",
-		Listed: "n"
-	  })
+		Listed: "n",
+	})
 	carData = append(carData, Car{
-		No: "4",
+		No: 4,
 		Vin: "G3SBUMF080FW470449",
 		Model: "Civic",
 		Make: "Honda",
-		Year: "2016",
-		MSRP: "140,000",
+		Year: 2016,
+		MSRP: 140000,
 		Status: "sold",
 		Booked: "n",
-		Listed: "n"
-	  })
+		Listed: "n",
+	})
 
 	// Route Handlers / Endpoints
 	r.HandleFunc("/inventory", getInventory).Methods("GET")
-	r.HandleFunc("/commission", getCommission).Methods("GET")
-	r.HandleFunc("/managemarket", getManageMarket).Methods("GET")
-	r.HandleFunc("/managecustomer", getManageCustomer).Methods("GET")
-	r.HandleFunc("/reportsetting", getReportSetting).Methods("GET")
+	// r.HandleFunc("/commission", getCommission).Methods("GET")
+	// r.HandleFunc("/managemarket", getManageMarket).Methods("GET")
+	// r.HandleFunc("/managecustomer", getManageCustomer).Methods("GET")
+	// r.HandleFunc("/reportsetting", getReportSetting).Methods("GET")
 	
 	http.ListenAndServe(":8080", r)
 }
