149044471434587bf3d4bb0da1ccb8bb6e20b1c0
LeeYoungMin <miniyoung37@gmail.com>
2020-02-07 17:33:14 +0900

    add inventory struct and route endpoint

--- src/main.go
+++ src/main.go
@@ -7,6 +7,29 @@ import (
 	"github.com/gorilla/mux"
 )
 
+// Model
+type Inventory struct{
+	No 		int 	`json:"no"`
+	Vin 	string 	`json:"vin"`
+	Model 	string 	`json:"model"`
+	Make 	string 	`json:"make"`
+	Year 	int 	`json:"year"`
+	MSRP 	int 	`json:"msrp"`
+	Status 	string 	`json:"status"`
+	Booked 	string 	`json:"booked"`
+	Listed 	string 	`json:"listed"`
+}
+
 func main(){
+	// Init Router
+	r := mux.NewRouter()
+
+	// Route Handlers / Endpoints
+	r.HandleFunc("/inventory", getInventory).Methods("GET")
+	r.HandleFunc("/commission", getCommission).Methods("GET")
+	r.HandleFunc("/managemarket", getManageMarket).Methods("GET")
+	r.HandleFunc("/managecustomer", getManageCustomer).Methods("GET")
+	r.HandleFunc("/reportsetting", getReportSetting).Methods("GET")
 	
+	http.ListenAndServe(":8080", r)
 }
